db5c104350c1416a3373744ceb0e7baf81b6a1a8
malarvizhi2007 <malarvizhi17747@gmail.com>
2026-03-13 09:45:17 +0530

    UC1:Application Startup

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,12 @@
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version 1.0");
+        System.out.println("=================================");
+
+    }
 }
